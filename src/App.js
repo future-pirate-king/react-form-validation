aec6b9ab7e60a54f09a91adfa8a9db3ccbf4051b
Sam David <32875617+future-pirate-king@users.noreply.github.com>
2020-11-20 15:17:11 +0530

    form handling and validation

--- src/App.js
+++ src/App.js
@@ -16,6 +16,8 @@ import VisibilityOff from "@material-ui/icons/VisibilityOffRounded";
 import Visibility from "@material-ui/icons/VisibilityRounded";
 import clsx from "clsx";
 import { withStyles, makeStyles } from "@material-ui/core/styles";
+import { Form, Formik } from "formik";
+import * as yup from "yup";
 
 const useStyles = makeStyles((theme) => ({
   labelRoot: {
@@ -34,96 +36,233 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
+const validationSchema = yup.object().shape({
+  fullName: yup
+    .string()
+    .min(2, "Name is too short!")
+    .max(50, "Name is too long!")
+    .required("Name is required!"),
+  password: yup
+    .string()
+    .min(8, "Password length should be between 8-16")
+    .max(16, "Password length should be between 8-16")
+    .required("Password is required!"),
+  email: yup.string().email("Enter valid email").required("Email is required!"),
+  phoneNumber: yup
+    .string()
+    .length(10, "Enter valid phone number!")
+    .required("Phone number is required!"),
+  salary: yup.number().required("Salary is required!")
+});
+
+const initialValues = {
+  fullName: "Sam David",
+  email: "",
+  password: "",
+  phoneNumber: "",
+  gender: "male",
+  address: {
+    line1: "495/8, Sanjay nagar",
+    line2: "PG Road, Kandivali west",
+    landmark: "Opposite to PG",
+    city: "Mumbai",
+    state: "Maharashtra",
+    pinCode: "400067"
+  },
+  salary: ""
+};
+
 export default function App() {
   const classes = useStyles();
   const [showPassword, setShowPassword] = useState(false);
+
+  const onSubmit = (values) => {
+    console.log(values);
+  };
 
   return (
     <Box p={1} maxWidth="900px" margin="0 auto">
       <Box>
         <Typography variant="h5">Registration</Typography>
       </Box>
-      <Box
-        display="flex"
-        style={{ gap: 24 }}
-        flexWrap="wrap"
-        py={2}
-        justifyContent="space-between"
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={onSubmit}
       >
-        <StyledTextField variant="outlined" label="Full name" />
-        <StyledTextField variant="outlined" label="E-mail" />
-        <StyledTextField
-          variant="outlined"
-          label="Password"
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={() => setShowPassword(!showPassword)}
-                  onMouseDown={() => setShowPassword(!showPassword)}
-                >
-                  {showPassword ? <Visibility /> : <VisibilityOff />}
-                </IconButton>
-              </InputAdornment>
-            )
-          }}
-          type={showPassword ? "text" : "password"}
-        />
-        <StyledTextField
-          variant="outlined"
-          label="Phone number"
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">+91</InputAdornment>
-            )
-          }}
-        />
-        <FormControl component="fieldset" style={{ width: "100%" }}>
-          <FormLabel component="legend">Gender</FormLabel>
-          <RadioGroup
-            className={classes.radioGroup}
-            aria-label="gender"
-            name="gender1"
-          >
-            {[
-              { value: "female", label: "Female" },
-              { value: "male", label: "Male" },
-              { value: "other", label: "Other" }
-            ].map(({ value, label }) => (
-              <FormControlLabel
-                className={classes.labelRoot}
-                value={value}
-                control={<StyledRadio color="primary" />}
-                label={label}
-              />
-            ))}
-          </RadioGroup>
-        </FormControl>
-        <FormControl component="fieldset" style={{ width: "100%" }}>
-          <FormLabel component="legend">Address</FormLabel>
-          <Box pt={1.5} display="flex" flexWrap="wrap" style={{ gap: 24 }}>
-            <StyledTextField variant="outlined" label="Address line 1" />
-            <StyledTextField variant="outlined" label="Address line 2" />
-            <StyledTextField variant="outlined" label="Landmark" />
-            <StyledTextField variant="outlined" label="City" />
-            <StyledTextField variant="outlined" label="State" />
-            <StyledTextField
-              variant="outlined"
-              label="Pin code"
-              type="number"
-            />
-          </Box>
-        </FormControl>
-        <StyledTextField
-          variant="outlined"
-          label="Annual Salary"
-          InputProps={{
-            startAdornment: <InputAdornment position="start">Rs</InputAdornment>
-          }}
-        />
-        <StyledButton endIcon={<ArrowIcon />}>Submit</StyledButton>
-      </Box>
+        {({ values, touched, errors, handleChange, handleSubmit }) => {
+          return (
+            <Form onSubmit={handleSubmit}>
+              <Box
+                display="flex"
+                style={{ gap: 24 }}
+                flexWrap="wrap"
+                py={2}
+                justifyContent="space-between"
+              >
+                <StyledTextField
+                  name="fullName"
+                  value={values.fullName}
+                  variant="outlined"
+                  label="Full name"
+                  onChange={handleChange}
+                  error={errors.fullName && touched.fullName}
+                  helperText={
+                    errors.fullName && touched.fullName ? errors.fullName : ""
+                  }
+                />
+                <StyledTextField
+                  name="email"
+                  value={values.email}
+                  variant="outlined"
+                  label="E-mail"
+                  onChange={handleChange}
+                  error={errors.email && touched.email}
+                  helperText={errors.email && touched.email ? errors.email : ""}
+                />
+                <StyledTextField
+                  name="password"
+                  value={values.password}
+                  variant="outlined"
+                  label="Password"
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="toggle password visibility"
+                          onClick={() => setShowPassword(!showPassword)}
+                          onMouseDown={() => setShowPassword(!showPassword)}
+                        >
+                          {showPassword ? <Visibility /> : <VisibilityOff />}
+                        </IconButton>
+                      </InputAdornment>
+                    )
+                  }}
+                  type={showPassword ? "text" : "password"}
+                  onChange={handleChange}
+                  error={errors.password && touched.password}
+                  helperText={
+                    errors.password && touched.password ? errors.password : ""
+                  }
+                />
+                <StyledTextField
+                  name="phoneNumber"
+                  value={values.phoneNumber}
+                  variant="outlined"
+                  label="Phone number"
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">+91</InputAdornment>
+                    )
+                  }}
+                  onChange={handleChange}
+                  error={errors.phoneNumber && touched.phoneNumber}
+                  helperText={
+                    errors.phoneNumber && touched.phoneNumber
+                      ? errors.phoneNumber
+                      : ""
+                  }
+                />
+                <FormControl component="fieldset" style={{ width: "100%" }}>
+                  <FormLabel component="legend">Gender</FormLabel>
+                  <RadioGroup
+                    className={classes.radioGroup}
+                    aria-label="gender"
+                    name="gender"
+                    value={values.gender}
+                  >
+                    {[
+                      { value: "female", label: "Female" },
+                      { value: "male", label: "Male" },
+                      { value: "other", label: "Other" }
+                    ].map(({ value, label }, index) => (
+                      <FormControlLabel
+                        key={index}
+                        className={classes.labelRoot}
+                        value={value}
+                        control={<StyledRadio color="primary" />}
+                        label={label}
+                      />
+                    ))}
+                  </RadioGroup>
+                </FormControl>
+                <FormControl component="fieldset" style={{ width: "100%" }}>
+                  <FormLabel component="legend">Address</FormLabel>
+                  <Box
+                    pt={1.5}
+                    display="flex"
+                    flexWrap="wrap"
+                    style={{ gap: 24 }}
+                  >
+                    <StyledTextField
+                      name="line1"
+                      value={values.address.line1}
+                      readOnly
+                      variant="outlined"
+                      label="Address line 1"
+                    />
+                    <StyledTextField
+                      name="line2"
+                      value={values.address.line2}
+                      readOnly
+                      variant="outlined"
+                      label="Address line 2"
+                    />
+                    <StyledTextField
+                      name="landmark"
+                      value={values.address.landmark}
+                      readOnly
+                      variant="outlined"
+                      label="Landmark"
+                    />
+                    <StyledTextField
+                      name="city"
+                      value={values.address.city}
+                      readOnly
+                      variant="outlined"
+                      label="City"
+                    />
+                    <StyledTextField
+                      name="state"
+                      value={values.address.state}
+                      readOnly
+                      variant="outlined"
+                      label="State"
+                    />
+                    <StyledTextField
+                      name="pinCode"
+                      value={values.address.pinCode}
+                      readOnly
+                      variant="outlined"
+                      label="Pin code"
+                      type="number"
+                    />
+                  </Box>
+                </FormControl>
+                <StyledTextField
+                  name="salary"
+                  value={values.salary}
+                  variant="outlined"
+                  label="Annual Salary"
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">Rs</InputAdornment>
+                    )
+                  }}
+                  onChange={handleChange}
+                  error={errors.salary && touched.salary}
+                  helperText={
+                    errors.salary && touched.salary ? errors.salary : ""
+                  }
+                />
+                <StyledButton type="submit" endIcon={<ArrowIcon />}>
+                  Submit
+                </StyledButton>
+              </Box>
+            </Form>
+          );
+        }}
+      </Formik>
     </Box>
   );
 }
