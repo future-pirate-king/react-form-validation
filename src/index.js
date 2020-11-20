dbf4ad094745f0ce2fcfb6fe3f920e15e735a542
Sam David <32875617+future-pirate-king@users.noreply.github.com>
2020-11-20 11:22:36 +0530

    added dependency and theme

--- src/index.js
+++ src/index.js
@@ -3,10 +3,40 @@ import ReactDOM from "react-dom";
 
 import App from "./App";
 
+import { ThemeProvider } from "@material-ui/styles";
+import { createMuiTheme } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  overrides: {
+    MuiAppBar: {
+      colorPrimary: {
+        backgroundColor: "#FFFFFF"
+      }
+    }
+  },
+  palette: {
+    primary: {
+      main: "#967ADC",
+      light: "#AC92EC",
+      dark: "#967ADC",
+      contrastText: "#FFFFFF"
+    },
+    background: {
+      default: "#FFFFFF",
+      paper: "#FFFFFF"
+    }
+  },
+  typography: {
+    fontFamily: ["Nunito", "sans-serif"].join(",")
+  }
+});
+
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   rootElement
 );
